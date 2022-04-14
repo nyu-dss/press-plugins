fc264128792f3e766ac0dbde5d4b1dc55830a22c
f <f@sutty.nl>
2022-04-14 19:09:15 -0300

    generate an index for the panel
    so we can query data directly from github

--- lib/press-plugins.rb
+++ lib/press-plugins.rb
@@ -3,5 +3,6 @@
 require_relative 'press-plugins/contributors'
 require_relative 'press-plugins/default_availability'
 require_relative 'press-plugins/html_migration'
+require_relative 'press-plugins/panel_helper'
 require_relative 'press-plugins/remove_print_essays_from_index'
 require_relative 'press-plugins/reverse_markdown_extensions'
---
+++ lib/press-plugins/panel_helper.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Generate an index for the panel.  This file will be downloaded
+# directly from Github's repositories and is for internal use only.
+Jekyll::Hooks.register :site, :post_read, priority: :low do |site|
+  panel = {
+    posts: {},
+    layouts: {}
+  }
+
+  # Index everything by layout and relative path.
+  site.posts.docs.each do |doc|
+    # Relative path is for downloading
+    # Title is for humans
+    # Layout is for collections
+    # and URL for linking back to the website
+    panel[:posts][doc.relative_path] = {
+      title: doc.data['title'],
+      layout: doc.data['layout'],
+      url: doc.url
+    }
+
+    panel[:layouts][doc.data['layout']] ||= []
+    panel[:layouts][doc.data['layout']]  << doc.relative_path
+  end
+
+  # The file is later available to themes.
+  File.open(site.in_source_dir(site.config['data_dir'], 'panel.json'), 'w') do |f|
+    Jekyll.logger.info 'Panel', "Creating #{f.path}"
+
+    f.write panel.to_json
+  end
+end
